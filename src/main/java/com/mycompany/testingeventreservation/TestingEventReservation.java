c4c608973dc386ddb189de7ef84e72326f4bc1c6
RC_Student_lab <RC_Student_lab@10.0.1.23>
2024-10-16 08:44:51 +0000

    methods for testing the code

---
+++ src/main/java/com/mycompany/testingeventreservation/TestingEventReservation.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testingeventreservation;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+ ;
+public class TestingEventReservation {
+
+    public static void main(String[] args) {
+        
+    public void testCheckbookingCode(){
+        System.out.println("checkbookingCode");
+        boolean expResult = false;
+        boolean result = EventReservation.checkUserName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of checkPasswordComplexity method, of class Login.
+     */
+    
+    public void testCheckPasswordComplexity() {
+        System.out.println("checkPasswordComplexity");
+        boolean expResult = false;
+        boolean result = EventReservation.checkPasswordComplexity();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    }
+
